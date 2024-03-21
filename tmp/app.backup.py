0d8c3933d27ed00f7093af85dad9fb65a547ca42
Your Name <color275@gmail.com>
2024-03-21 23:27:57 +0900

    modify

--- tmp/app.backup.py
+++ tmp/app.backup.py
@@ -1,8 +1,11 @@
-# export AWS_DEFAULT_REGION='us-west-2'
-# nohup streamlit run app.py --server.port 8502 &
+# 질문 리스트
+# 오늘 주문실적을 알려줘. 최근 주문시간 부터 확인할께
+# 가장 적게 팔린 상품을 알려줘
+# 오늘 상품별 판매액 rank 5 를 알려줘
+
 
 import streamlit as st
-import fitz
+import fitz  
 import logging
 import boto3
 import json
@@ -20,85 +23,78 @@ from langchain.prompts import PromptTemplate
 from langchain_community.llms.bedrock import Bedrock
 from opensearchpy import OpenSearch, RequestsHttpConnection
 from opensearchpy.helpers import bulk
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-INIT_MESSAGE = {"role": "assistant",
+INIT_MESSAGE = {"role": "assistant", 
                 "type": "text",
-                "content": """
-안녕하세요. 저는 <font color='red'><b>Amazon Bedrock과 Claude3</b></font>를 활용해서 여러분들이 찾고 싶은 데이터를 대신 찾아줄 <i><b>[데이터가 궁금해]<i><b> 입니다. 
-<br>아래와 같이 질문해보세요.
-- <font color='#32CD32;'><b>오늘 주문실적을 알려줘</b></font><br>
-- <font color='#32CD32;'><b>오늘 가장 적게 팔린 상품을 알려줘</b></font><br>
-- <font color='#32CD32;'><b>어제 판매된 상품 중 주문 금액 TOP 5 를 알려줘</b></font><br>
----
-무엇을 도와드릴까요?"""}
+                "content": """안녕하세요. 저는 <i><b>베드락의 Claude3</b></i> 입니다. 
+                                                   <br>현재 <font color='#32CD32;'><b>손흥민</b></font>과 <font color='#32CD32;'><b>AWS EC2</b></font>에 대해 학습되어 있고 
+                                                   왼쪽 섹션에서 <font color='red'><b>PDF 업로드</b></font>를 통해 추가 학습을 할 수 있습니다. <br>무엇을 도와드릴까요?"""}
 
-CONTEXT_DATA = """
-        참고. 안녕이라고 질문하면 인사와 간단한 소개만 해줘.
-        참고. 데이터를 알려달라고 할 때만 질문을 확인할 수 있는 SQL을 만들어서 제공해줘.
-        참고. 질문에 대해 이해한 내용을 앞에 작성해줘.
-        참고. SQL은 제공한 테이블만 사용해서 만들어줘
-        참고. SQL은 markdown의 코드 sql 태그 안에 넣어줘.
-        참고. markdown 코드 sql 태그 안에는 하나의 sql 만 넣어줘.
-        참고. SELECT 절의 컬럼 alias는 한글로 만들어줘.
-        참고. where 절에서 컬럼을 형변환을 위해 가공하지 말아줘. 예를 들어 order_dt 를 DATE(order_dt)로 변환하지 말아라.
-        참고. where 컬럼A = 상수B 일 떄 컬럼A와 상수B의 데이터타입이 다르다면 상수B를 형변환해서 데이터타입을 동일하게 맞춰줘.예를 들어 order_dt = CURDATE() 일 경우 order_dt = date_format(curdate(), '%Y-%m-%d') 로 사용해줘
-        참고. where 에서 동일한 데이터타입으로 비교를 할 수 있게 적절한 형변환을 해줘.
-        참고. SQL 생성 시 최대 10개의 레코드만 표시되도록 Limit 10을 SQL에 포함시켜줘. 그리고 Limit 10을 임의로 추가했다고 초록색 글자로 안내해줘.
-        참고. SQL 생성 시 별도의 정렬이 필요없다면 [alias].last_update_time desc 을 정렬 조건으로 추가해줘. [alias] 에는 적절한 테이블의 alias를 명시해줘.
-        """
+# 참고. 특히 order_dt 는 문자열이므로 비교되는 값도 문자열이어야 한다.
+# 참고. SQL은 sql code 태그를 사용해서 표시해주고, 하나의 SQL만 작성해줘
+# 참고. where 컬럼A = 상수B 일 떄 컬럼A와 상수B의 데이터타입이 다르다면 상수B를 형변환해서 데이터타입을 동일하게 맞춰줘.예를 들어 order_dt = CURDATE() 일 경우 order_dt = date_format(curdate(), '%y-%m-%d') 로 사용해줘
+PROMPT_HELP = """
+
+참고. 데이터 질의가 필요한 것만 SQL을 만들어줘
+참고. 질문을 확인할 수 있는 SQL을 만들 수 있다면 SQL를 작성해줘.
+참고. SQL은 markdown의 코드 sql 태그 안에 넣어줘.
+참고. markdown 코드 sql 태그 안에는 하나의 sql 만 넣어줘.
+참고. SELECT 절의 컬럼 alias 는 대도록 한글로 만들어줘.
+참고. where 절에서 컬럼을 형변환을 위해 가공하지 말아줘. 예를 들어 order_dt 를 DATE(order_dt)로 변환하지 말아라.
+참고. where 컬럼A = 상수B 일 떄 컬럼A와 상수B의 데이터타입이 다르다면 상수B를 형변환해서 데이터타입을 동일하게 맞춰줘.예를 들어 order_dt = CURDATE() 일 경우 order_dt = date_format(curdate(), '%Y-%m-%d') 로 사용해줘
+참고. where 에서 동일한 데이터타입으로 비교를 할 수 있게 적절한 형변환을 해줘.
+참고. SQL 생성 시 최대 10개의 레코드만 표시되도록 Limit 10을 SQL에 포함시켜줘. 그리고 Limit 10을 임의로 추가했다고 초록색 글자로 안내해줘.
+참고. SQL 생성 시 별도의 정렬이 필요없다면 last_update_time desc 을 정렬 조건으로 추가해줘.
+
+
+"""
 ################################################################################
 
 load_dotenv()
 opensearch_username = os.getenv('OPENSEARCH_USERNAME')
 opensearch_password = os.getenv('OPENSEARCH_PASSWORD')
 opensearch_endpoint = os.getenv('OPENSEARCH_ENDPOINT')
-index_name = os.getenv('OPENSEARCH_INDEX_NAME')
-mysql_host = os.getenv('MYSQL_HOST')
-mysql_port = os.getenv('MYSQL_PORT')
-mysql_user = os.getenv('MYSQL_USER')
-mysql_password = os.getenv('MYSQL_PASSWORD')
-mysql_db = os.getenv('MYSQL_DB')
-
+index_name = 'index_chatbot_test_04'
 bedrock_region = 'us-west-2'
 stop_record_count = 100
 record_stop_yn = False
 bedrock_model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
 bedrock_embedding_model_id = "amazon.titan-embed-text-v1"
+
+mysql_host = os.getenv('MYSQL_HOST')
+mysql_port = os.getenv('MYSQL_PORT')
+mysql_user = os.getenv('MYSQL_USER')
+mysql_password = os.getenv('MYSQL_PASSWORD')
+mysql_db = os.getenv('MYSQL_DB')
 ################################################################################
-
-
+    
 def get_opensearch_cluster_client():
     opensearch_client = OpenSearch(
         hosts=[{
             'host': opensearch_endpoint,
             'port': 443
-        }],
+            }],
         http_auth=(opensearch_username, opensearch_password),
-        index_name=index_name,
+        index_name = index_name,
         use_ssl=True,
         verify_certs=True,
         connection_class=RequestsHttpConnection,
         timeout=30
-    )
+        )
     return opensearch_client
 
-
 def get_bedrock_client():
-    bedrock_client = boto3.client(
-        "bedrock-runtime", region_name=bedrock_region)
-    return bedrock_client
-
+    bedrock_client = boto3.client("bedrock-runtime", region_name=bedrock_region)
+    return bedrock_client    
 
 def create_langchain_vector_embedding_using_bedrock(bedrock_client):
     bedrock_embeddings_client = BedrockEmbeddings(
         client=bedrock_client,
         model_id=bedrock_embedding_model_id)
-    return bedrock_embeddings_client
-
+    return bedrock_embeddings_client    
 
 def create_opensearch_vector_search_client(bedrock_embeddings_client, _is_aoss=False):
     docsearch = OpenSearchVectorSearch(
@@ -110,30 +106,21 @@ def create_opensearch_vector_search_client(bedrock_embeddings_client, _is_aoss=F
     )
     return docsearch
 
-
 def create_bedrock_llm():
     # claude-2 이하
     # bedrock_llm = Bedrock(
-    #     model_id=model_version_id,
+    #     model_id=model_version_id, 
     #     client=bedrock_client,
     #     model_kwargs={'temperature': 0}
     #     )
     # bedrock_llm = BedrockChat(model_id=model_version_id, model_kwargs={'temperature': 0}, streaming=True)
 
-    bedrock_llm = BedrockChat(
-        model_id=bedrock_model_id, 
-        model_kwargs={'temperature': 0},
-        streaming=True,
-        callbacks=[StreamingStdOutCallbackHandler()]
-        )
+    bedrock_llm = BedrockChat(model_id=bedrock_model_id, model_kwargs={'temperature': 0})
     return bedrock_llm
 
-
 def get_bedrock_client():
-    bedrock_client = boto3.client(
-        "bedrock-runtime", region_name=bedrock_region)
+    bedrock_client = boto3.client("bedrock-runtime", region_name=bedrock_region)
     return bedrock_client
-
 
 def create_vector_embedding_with_bedrock(text, bedrock_client):
     payload = {"inputText": f"{text}"}
@@ -150,13 +137,11 @@ def create_vector_embedding_with_bedrock(text, bedrock_client):
     embedding = response_body.get("embedding")
     return {"_index": index_name, "text": text, "vector_field": embedding}
 
-
-def extract_sentences_from_pdf(opensearch_client, pdf_file, progress_bar, progress_text):
-    try:
-        logging.info(
-            f"Checking if index {index_name} exists in OpenSearch cluster")
-
-        exists = opensearch_client.indices.exists(index=index_name)
+def extract_sentences_from_pdf(opensearch_client, pdf_file, progress_bar, progress_text):    
+    try :
+        logging.info(f"Checking if index {index_name} exists in OpenSearch cluster")
+        
+        exists = opensearch_client.indices.exists(index=index_name)               
 
         if not exists:
             body = {
@@ -187,7 +172,7 @@ def extract_sentences_from_pdf(opensearch_client, pdf_file, progress_bar, progre
                     body=body
                 )
 
-        doc = fitz.open(stream=pdf_file.read(), filetype="pdf")
+        doc = fitz.open(stream=pdf_file.read(), filetype="pdf")    
         all_records = []
         for page in doc:
             all_records.append(page.get_text())
@@ -204,84 +189,69 @@ def extract_sentences_from_pdf(opensearch_client, pdf_file, progress_bar, progre
 
         total_records = len(all_records)
         processed_records = 0
-
+        
         bedrock_client = get_bedrock_client()
 
         all_json_records = []
 
         for record in all_records:
             if record_stop_yn and processed_records > stop_record_count:
-
+                
                 success, failed = bulk(opensearch_client, all_json_records)
                 break
-
-            records_with_embedding = create_vector_embedding_with_bedrock(
-                record, bedrock_client)
+            
+            records_with_embedding = create_vector_embedding_with_bedrock(record, bedrock_client)
             all_json_records.append(records_with_embedding)
-
+            
             processed_records += 1
             progress = int((processed_records / total_records) * 100)
             progress_bar.progress(progress)
-
+            
             if processed_records % 500 == 0 or processed_records == len(all_records):
-
+                
                 success, failed = bulk(opensearch_client, all_json_records)
-                all_json_records = []
-
+                all_json_records = []  
+        
         progress_text.text("완료")
         logging.info("임베딩을 사용하여 레코드 생성 완료")
-
+        
         return total_records
     except Exception as e:
         print(str(e))
         st.error('PDF를 임베딩 하는 과정에서 오류가 발생되었습니다.')
         return 0
 
-
 def find_answer_in_sentences(question):
-    try:
-        question = question
+    try :        
+        question = question + PROMPT_HELP
         bedrock_client = get_bedrock_client()
         bedrock_llm = create_bedrock_llm()
-
-        bedrock_embeddings_client = create_langchain_vector_embedding_using_bedrock(
-            bedrock_client)
-
-        opensearch_vector_search_client = create_opensearch_vector_search_client(
-            bedrock_embeddings_client)
         
+        bedrock_embeddings_client = create_langchain_vector_embedding_using_bedrock(bedrock_client)
         
-        prompt_template = """
-        Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. don't include harmful content        
+        opensearch_vector_search_client = create_opensearch_vector_search_client(bedrock_embeddings_client)
+        
+        prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. don't include harmful content
+
         {context}
 
         Question: {question}
         Answer:"""
-        
-        prompt_template = PromptTemplate(
+        PROMPT = PromptTemplate(
             template=prompt_template, input_variables=["context", "question"]
         )
-
-        prompt = prompt_template.format(
-            context=CONTEXT_DATA, question=question)
+        logging.info(f"Starting the chain with KNN similarity using OpenSearch, Bedrock FM {bedrock_model_id}, and Bedrock embeddings with {bedrock_embedding_model_id}")
         
-        print("# prompt : ", prompt)
-
-
-        logging.info(
-            f"Starting the chain with KNN similarity using OpenSearch, Bedrock FM {bedrock_model_id}, and Bedrock embeddings with {bedrock_embedding_model_id}")
-
-        qa = RetrievalQA.from_chain_type(llm=bedrock_llm,
-                                         chain_type="stuff",
-                                         retriever=opensearch_vector_search_client.as_retriever(),
-                                         return_source_documents=True,
-                                         chain_type_kwargs={
-                                             "prompt": prompt_template, "verbose": True},
-                                         verbose=True)
-
-        response = qa({"context": CONTEXT_DATA, "query": question},
-                      return_only_outputs=False)
-
+        qa = RetrievalQA.from_chain_type(llm=bedrock_llm, 
+                                        chain_type="stuff", 
+                                        retriever=opensearch_vector_search_client.as_retriever(),
+                                        return_source_documents=True,
+                                        chain_type_kwargs={"prompt": PROMPT, "verbose": True},
+                                        verbose=True)
+        
+        
+        response = qa(question, return_only_outputs=False)
+        
         source_documents = response.get('source_documents')
         # logging.info(f"The answer from Bedrock {bedrock_model_id} is: {response.get('result')}")
         return f"{response.get('result')}"
@@ -345,9 +315,8 @@ def execute_query_and_return_df(sql):
         conn.close()
     return df
 
-
-def main():
-
+def main():    
+    
     # 기존 업로드 문서 삭제
     # if st.sidebar.button("기존 업로드 문서 삭제"):
     #     response = opensearch_client.delete_opensearch_index(opensearch_client, index_name)
@@ -355,60 +324,59 @@ def main():
     #     if response:
     #         logging.info("OpenSearch index successfully deleted")
     #         st.sidebar.success("OpenSearch 인덱스가 성공적으로 삭제되었습니다.")  # 성공 알림 추가
-
+    
     opensearch_client = get_opensearch_cluster_client()
     st.set_page_config(page_title='🤖 Chat with Bedrock', layout='wide')
-    # st.header('_Chatbot_ using :blue[OpenSearch] :sunglasses:', divider='rainbow')
-    st.header(':blue[데이터가] _궁금해_ :sunglasses:', divider='rainbow')    
+    st.header('_Chatbot_ using :blue[OpenSearch] :sunglasses:', divider='rainbow')
+    
 
     if 'qa_history' not in st.session_state:
         st.session_state.qa_history = []
 
     with st.sidebar:
-        st.sidebar.markdown(
-            ':smile: **Createby:** chiholee@amazon.com', unsafe_allow_html=True)
+        st.sidebar.markdown(':smile: **Createby:** chiholee@amazon.com', unsafe_allow_html=True)
         st.sidebar.markdown('---')
         st.title("RAG Embedding")
-        pdf_file = st.file_uploader(
-            "PDF 업로드를 통해 추가 학습을 할 수 있습니다.", type=["pdf"], key=None)
+        pdf_file = st.file_uploader("ChatBot이 학습할 PDF 업로드", type=["pdf"], key=None)
         
-
         if 'last_uploaded' not in st.session_state:
             st.session_state.last_uploaded = None
 
         if pdf_file is not None and pdf_file != st.session_state.last_uploaded:
-            progress_text = st.empty()
+            progress_text = st.empty() 
             st.session_state['progress_bar'] = st.progress(0)
             progress_text.text("RAG(OpenSearch) 임베딩 중...")
-            record_cnt = extract_sentences_from_pdf(
-                opensearch_client, pdf_file, st.session_state['progress_bar'], progress_text)
-            if record_cnt > 0:
-                st.session_state['processed'] = True
-                st.session_state['record_cnt'] = record_cnt
-                st.session_state['progress_bar'].progress(100)
+            record_cnt = extract_sentences_from_pdf(opensearch_client, pdf_file, st.session_state['progress_bar'], progress_text)
+            if record_cnt > 0 :
+                st.session_state['processed'] = True  
+                st.session_state['record_cnt'] = record_cnt  
+                st.session_state['progress_bar'].progress(100)  
                 st.session_state.last_uploaded = pdf_file
                 st.success(f"{record_cnt} Vector 임베딩 완료!")
+        
+    
 
     if "messages" not in st.session_state.keys():
         st.session_state.messages = [INIT_MESSAGE]
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
-            if message["type"] == "text":
+            if message["type"] == "text" :
                 st.markdown(message["content"], unsafe_allow_html=True)
-            elif message["type"] == "data":
+            elif message["type"] == "data" :
                 st.markdown(message["content"][0], unsafe_allow_html=True)
                 st.write(':bulb: **쿼리 실행 결과**', message["content"][1])
 
     question = st.chat_input("Say something")
 
-    if question:
-        st.session_state.messages.append({"role": "user",
+    if question :
+        st.session_state.messages.append({"role": "user", 
                                           "type": "text",
                                           "content": question})
         with st.chat_message("user"):
             st.markdown(question, unsafe_allow_html=True)
-
+            
+    
     if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
@@ -417,22 +385,35 @@ def main():
                 sql_queries = re.findall(r'```sql(.*?)```', answer, re.DOTALL)
                 st.markdown(answer, unsafe_allow_html=True)
 
-                if len(sql_queries) > 0:
+                
+                
+
+                if len(sql_queries) > 0 :
                     # st.markdown(sql_queries[0], unsafe_allow_html=True)
                     sql = sql_queries[0]
                     df = execute_query_and_return_df(sql)
                     st.write(':bulb: **쿼리 실행 결과**', df)
 
-                    message = {"role": "assistant",
+                    message = {"role": "assistant", 
                                "type": "data",
-                               "content": [answer, df]}
-
-                else:
+                               "content": [answer,df]}
+                    
+                
+                else :
                     message = {"role": "assistant",
                                "type": "text",
                                "content": answer}
-
+                    
+                
                 st.session_state.messages.append(message)
+
+
+
+
+
+
+        
+
 
 
 if __name__ == "__main__":
